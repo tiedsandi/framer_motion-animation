26176dd3378f183d79405d99991949036538d80c
tiedsandi <fachransandi@gmail.com>
2023-12-02 08:50:37 +0700

    Implementaion Animation: - implemented useAnimationControls - implemented useInView - implemented useScroll & useSpring

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import "./App.css";
-import BasicOfMotion from "./components/BasicOfMotion";
-import Gestures from "./components/Gestures";
-import AnimationControls from "./components/AnimationControls";
-import ViewBasedAnimations from "./components/ViewBasedAnimations";
-import ScrollAnimations from "./components/ScrollAnimations";
+import './App.css';
+import BasicOfMotion from './components/BasicOfMotion';
+import Gestures from './components/Gestures';
+import AnimationControls from './components/AnimationControls';
+import ViewBasedAnimations from './components/ViewBasedAnimations';
+import ScrollAnimations from './components/ScrollAnimations';
 
 function App() {
   return (
     <div>
       {/* <BasicOfMotion /> */}
-      <Gestures />
+      {/* <Gestures /> */}
       {/* <AnimationControls /> */}
       {/* <ViewBasedAnimations /> */}
-      {/* <ScrollAnimations /> */}
+      <ScrollAnimations />
     </div>
   );
 }
--- src/components/ViewBasedAnimations.jsx
+++ src/components/ViewBasedAnimations.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, {useEffect, useRef} from 'react';
+import {motion, useInView} from 'framer-motion';
 
 const ViewBasedAnimations = () => {
-  return <div>ViewBasedAnimations</div>;
+  const ref = useRef(null);
+  const isInView = useInView(ref, {once: true});
+
+  useEffect(() => {
+    console.log('Is in view -> ', isInView);
+  }, [isInView]);
+
+  return (
+    <>
+      <div style={{height: '150vh'}} />
+      <motion.div
+        style={{height: '100vh', background: 'black'}}
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        transition={{duration: 1}}
+      />
+      <div
+        ref={ref}
+        style={{
+          height: '100vh',
+          background: isInView ? 'blue' : 'red',
+          transition: '1s background',
+        }}
+      />
+    </>
+  );
 };
 
 export default ViewBasedAnimations;
